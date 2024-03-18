f11c6c11ad6c59eaaa7b82fdd1aba4aaf419435d
Paul Wei <weiliping.cn@gmail.com>
2024-03-18 23:07:16 +0800

    add a solution in python

--- src/main/py/leetcode/2997.使数组异或和等于-k-的最少操作次数.py
+++ src/main/py/leetcode/2997.使数组异或和等于-k-的最少操作次数.py
@@ -7,6 +7,22 @@ from typing import List
 # @lc code=start
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+        s = nums[0]
+        for i in range(1, len(nums)):
+            s ^= nums[i]
+        def to_bin(num: int) -> int:
+            bin_num = [0] * 20
+            for i in range(20):
+                if ((num >> i) & 1) == 1:
+                    bin_num[i] = 1
+                    continue
+            return bin_num
         
+        a, b = to_bin(s), to_bin(k)
+        cnt = 0
+        for i in range(20):
+            if a[i] != b[i]:
+                cnt += 1
+        return cnt
 # @lc code=end
 
