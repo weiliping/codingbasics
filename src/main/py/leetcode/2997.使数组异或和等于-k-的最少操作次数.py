fff100c092671b9226e4c3c4d3928aa5691f6165
Paul Wei <weiliping.cn@gmail.com>
2024-03-18 22:44:38 +0800

    add a solution in python

---
+++ src/main/py/leetcode/2997.使数组异或和等于-k-的最少操作次数.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=2997 lang=python3
+#
+# [2997] 使数组异或和等于 K 的最少操作次数
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+
